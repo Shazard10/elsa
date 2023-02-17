3221a0be32e643d1ae862760d8e3053212bbb82e
Shanid TG <109721076+Shazard10@users.noreply.github.com>
2023-02-17 14:58:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -476,16 +476,6 @@ async def settings(client, message):
                     callback_data=f'setgs#auto_delete#{settings["auto_delete"]}#{grp_id}',
                 ),
             ],
-            [
-                InlineKeyboardButton(
-                    'ShortLink',
-                    callback_data=f'setgs#is_shortlink#{settings["is_shortlink"]}#{grp_id}',
-                ),
-                InlineKeyboardButton(
-                    '✅ ON' if settings["is_shortlink"] else '❌ OFF',
-                    callback_data=f'setgs#is_shortlink#{settings["is_shortlink"]}#{grp_id}',
-                ),
-            ],
         ]
 
         reply_markup = InlineKeyboardMarkup(buttons)
